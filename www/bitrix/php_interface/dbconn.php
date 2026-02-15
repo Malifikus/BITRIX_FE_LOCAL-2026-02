40b79da7c9773a23472a5014e124f3ec5f64a06e
Malifikus <Vladikchelny@mail.ru>
2026-02-15 22:47:14 +0300

    afterbitrixsetup#1

--- www/bitrix/php_interface/dbconn.php
+++ www/bitrix/php_interface/dbconn.php
@@ -1,12 +1,11 @@
 <?php
-
-define("DBPersistent", false);
-$DBType = "mysql";
-$DBHost = "mysql";
-$DBLogin = "dev";
-$DBPassword = "dev";
-$DBName = "dev";
-
 $DBDebug = false;
 $DBDebugToFile = false;
-?>
+
+define("BX_FILE_PERMISSIONS", 0644);
+define("BX_DIR_PERMISSIONS", 0755);
+@umask(~(BX_FILE_PERMISSIONS | BX_DIR_PERMISSIONS) & 0777);
+
+define("BX_DISABLE_INDEX_PAGE", true);
+
+mb_internal_encoding("UTF-8");
